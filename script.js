133b1bb0d2cc79e305afcb77102ec5664e394602
Clover <117549805+CloverTheHacker@users.noreply.github.com>
2023-02-23 13:54:30 +0000

    Goooooooooddddd

---
+++ script.js
@@ -0,0 +1,28 @@
+function hola(){
+var fn = prompt("What is your name?");
+var pass = "Clover";
+var pass2 = "00253347";
+var CONFPT = "032207)#@@)&";
+if (fn === pass) {
+  var pass1 = prompt("What is your password?");
+  if (pass1 === pass2) {
+    alert("Thank you, " + fn + "!");
+  } else {
+    alert("Sorry, incorrect.");
+    window.open("Https://www.google.com/heart", "_self");
+  }
+} else {
+  alert("Unfortunatley, you aren't allowed. Sorry.");
+  window.open("Https://www.google.com/heart", "_self");
+}
+function CONF() {
+  var CONFP = prompt("CONF DATA ENTER PASS");
+  if (CONFP === CONFPT) {
+    alert("ACCEPTED");
+    window.open("https://0v3rseer.editorx.io/theimaginaryproject", "_self");
+  } else {
+    alert("DECLINED");
+    window.open("https://www.google.com/heart", "_self");
+  }
+}
+}
